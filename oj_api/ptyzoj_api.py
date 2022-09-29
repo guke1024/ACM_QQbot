4335ea54797410966f339f12d88ca8d26f896da1
pt3155420267 <33685002+pt3155420267@users.noreply.github.com>
2022-09-29 13:13:21 +0800

    Update ptyzoj_api.py

--- oj_api/ptyzoj_api.py
+++ oj_api/ptyzoj_api.py
@@ -65,7 +65,9 @@ class PTYZOJ(Contest):
               )
         return res
 
-
+    async def get_rating(self, name):
+        return -1
+    
     async def update_local_contest(self):
         return True
 
